540c9f9de95db6c05106f5bedd9a4c0bf1cdc297
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2018-01-13 18:16:41 -0500

    commenting

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -11,11 +11,17 @@ int main(int argc, char** argv) {
 	std::cout << "\n\nWELCOME TO THE CIRCLE PROGRAM!!\n\n--------------------\n\n";
 	std::cout << "RULES:\n";
 	std::cout << "1. Enter the radius of a circle.\n2. Negative values quit the program.\n\n";
+	
+	// Circle radius.
 	double radius = 0.0;
+	// Circle diameter.
 	double diameter;
+	// Circle area.
 	float area;
+	
 	std::cout << "--------------------\n\nRadius: ";
         std::cin >> radius;
+	
 	// Used code structure to check that the std::cin would not fail with
 	// an incorrect input value from the following stackoverflow answer:
 	// https://stackoverflow.com/a/18728791
@@ -25,13 +31,19 @@ int main(int argc, char** argv) {
 		std::cin.ignore(256, '\n');
 		std::cin >> radius;
 	}
+	
+	// Start loop for input. 
 	while (radius >= 0) {
 		diameter = radius * 2;
 		area = 3.14 * pow(radius, 2);
+		
 		std::cout << "\nArea: " + std::to_string(area) + "\n";
 		std::cout << "\nDiameter: " + std::to_string(diameter) + "\n";
 		std::cout << "--------------------\n\nRadius: ";
 		std::cin >> radius;
+		
+		// Error handling if input is not a float.
+		// See comment above for source.
 		while (std::cin.fail()) {
                 std::cout << "\nInteger values only allowed in this program.\nPlease enter a valid number: ";
                 std::cin.clear();
